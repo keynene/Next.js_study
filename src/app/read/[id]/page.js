518fdb13c2200098a13731abeed20a5038057dca
keynene <nonung2@naver.com>
2024-12-21 12:01:23 +0000

    db.json: json서버로부터 데이터 가져오기 (서버 컴포넌트)

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,8 +1,12 @@
-export default function Read(props){
+/* 사용자와 상호작용하지 않기 때문에 서버컴포넌트로 동작한다 */
+
+export default async function Read(props){
+  const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+  const topic = await resp.json();
   return(
     <>
-      <h2>Read</h2>
-      parameters : {props.params.id}
+      <h2>{topic.title}</h2>
+      {topic.body}
     </>
   )
 }
