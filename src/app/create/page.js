ad52e205f56e4a92f4b75fdc0166193e2fa55332
keynene <nonung2@naver.com>
2024-12-22 19:11:43 +0900

    POST: db.json에 POST요청하기 (client components)

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,5 +1,45 @@
+
+"use client"
+
+import { useRouter } from "next/navigation";
+
+  //onSubmit이 클라이언트 컴포넌트에서만 사용가능하기 때문
 export default function Create(){
+  const router = useRouter();
+
   return(
-    <>Create!!</>
+    <form onSubmit={(e)=>{
+      e.preventDefault();
+
+      //e.target을 하면 target이 form태그를 가리키게 됨
+      const title = e.target.title.value;
+      const body = e.target.body.value;
+
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({title,body})
+      }
+
+      fetch(`http://localhost:9999/topics`, options)
+        .then(res=>res.json())
+        .then(result=>{
+          console.log(result);
+          const lastid = result.id;
+          router.push(`/read/${lastid}`)
+        })
+    }}>
+      <p>
+        <input type="text" name="title" placeholder="title" />
+      </p>
+      <p>
+        <textarea name="body" placeholder="body" />
+      </p>
+      <p>
+        <input type="submit" value="create" />
+      </p>
+    </form>
   )
 }
