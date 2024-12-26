87de1c6de9f303adb68e8090aff449069a5ff2c8
keynene <nonung2@naver.com>
2024-12-26 18:19:27 +0900

    update&delete: 글 삭제 및 수정 기능 구현

--- src/app/Control.js
+++ src/app/Control.js
@@ -1,8 +1,9 @@
 "use client"
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 export default function Control(){
+  const router = useRouter();
   const params = useParams();
   const id = params.id;
 
@@ -12,7 +13,15 @@ export default function Control(){
       { id && (
         <> 
           <li><Link href={`/update/${id}`}>Update</Link></li>
-          <li><input type="button" value="delete" /></li>
+          <li><input type="button" value="delete" onClick={()=>{
+            const options = {method:'DELETE'}
+            fetch(`http://localhost:9999/topics/${id}`, options)
+              .then(resp => resp.json)
+              .then(result => {
+                router.push('/');
+                router.refresh();
+              })
+          }} /></li>
         </>
       ) }
       
--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,7 +1,7 @@
 /* 사용자와 상호작용하지 않기 때문에 서버컴포넌트로 동작한다 */
 
 export default async function Read(props){
-  const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+  const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`,{ cache: 'no-store' });
   const topic = await resp.json();
   return(
     <>
