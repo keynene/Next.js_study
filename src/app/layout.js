aa6f6a1b16d3e4a396781f12779bff0092d1225b
keynene <nonung2@naver.com>
2024-12-26 18:32:42 +0900

    env: 환경변수 설정하기, 보안의 중요성, example파일 (버전공유)

--- src/app/layout.js
+++ src/app/layout.js
@@ -31,7 +31,7 @@ export default async function RootLayout({ children }) {
   // },[])
 
   // const resp = await fetch('http://localhost:9999/topics');
-  const resp = await fetch('http://localhost:9999/topics', {cache: 'no-store'});
+  const resp = await fetch(process.env.NEXT_PUBLIC_API_URL+'topics', {cache: 'no-store'});
   const topics = await resp.json();
 
   return (
--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,7 +1,7 @@
 /* 사용자와 상호작용하지 않기 때문에 서버컴포넌트로 동작한다 */
 
 export default async function Read(props){
-  const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`,{ cache: 'no-store' });
+  const resp = await fetch(process.env.NEXT_PUBLIC_API_URL+`topics/${props.params.id}`,{ cache: 'no-store' });
   const topic = await resp.json();
   return(
     <>
